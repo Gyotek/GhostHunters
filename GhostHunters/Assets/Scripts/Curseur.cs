ba0a4f5f6f70fc5600914284b017541484fe7a3a
Gyotek <arthur.herbreteau@laposte.net>
2019-11-28 00:47:47 +0100

    Add curseur

---
+++ GhostHunters/Assets/Scripts/Curseur.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curseur : MonoBehaviour
+{
+    [SerializeField] Transform hitpointTr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * Mathf.Infinity, Color.blue);
+
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, 1 << 9))
+        {
+            Debug.Log("hit something");
+            Debug.DrawLine(transform.position, hit.point, Color.red);
+
+        }
+    }
+}
