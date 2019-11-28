40121417d33fdd045c185ac44b4155074d0e0d25
Gyotek <arthur.herbreteau@laposte.net>
2019-11-28 18:34:22 +0100

    Add Ciblage

---
+++ GhostHunters/Assets/Scripts/HitPoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitPoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Ghost>())
+        {
+            Debug.Log("hit a ghost");
+        }
+    }
+}
