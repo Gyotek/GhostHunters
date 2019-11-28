41ac354a7334e91bd0d8838bf5a8ee93d5559d78
SENAC Thomas <t.senac@rubika-edu.com>
2019-11-28 17:15:21 +0100

    events fantôme1

---
+++ GhostHunters/Assets/Scripts/GhostWasShot.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostWasShot : MonoBehaviour
+{
+    public Animator ghostAnimator;
+
+    public void GhostWasShotFunction()
+    {
+        ghostAnimator.SetBool("PhantomWasShot", true);
+    }
+}
