0c25eabbed58bdca6345cf0f4a6d3785a37cb572
SENAC Thomas <t.senac@rubika-edu.com>
2019-11-29 21:54:09 +0100

    flipped batch

---
+++ GhostHunters/Assets/Scripts/FlipSprite.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipSprite : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        spriteRenderer.flipX = true;
+    }
+}
