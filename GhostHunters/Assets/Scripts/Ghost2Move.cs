538b908d3cecea533f0d0295f0450e99869574a8
SENAC Thomas <t.senac@rubika-edu.com>
2019-11-29 11:07:42 +0100

    prefabs with movements done

---
+++ GhostHunters/Assets/Scripts/Ghost2Move.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Ghost2Move : MonoBehaviour
+{
+    public Transform destination;
+
+    bool canMove = false;
+
+    public float ghost2Speed;
+
+    private void Update()
+    {
+        Ghost2IsMoving();
+    }
+
+    private void Ghost2IsMoving()
+    {
+        if (canMove)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, destination.transform.position, ghost2Speed * Time.deltaTime);
+        }
+    }
+
+    public void Ghost2CanMove()
+    {
+        canMove = true;
+    }
+
+    public void Ghost2CantMove()
+    {
+        canMove = false;
+    }
+}
