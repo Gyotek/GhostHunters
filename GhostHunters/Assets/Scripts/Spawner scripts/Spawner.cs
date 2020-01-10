7ed9035e313b2f7ee35f96c6d72b8aea81577f57
SENAC Thomas <t.senac@rubika-edu.com>
2020-01-10 12:22:17 +0100

    added Odin Inspector

---
+++ GhostHunters/Assets/Scripts/Spawner scripts/Spawner.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] Ghosts;
+
+    public List<List<string>> letmeread = new List<List<string>>(2);
+}
