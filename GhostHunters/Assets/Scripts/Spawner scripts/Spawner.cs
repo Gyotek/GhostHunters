90ff4ea8eef7eadb9980ba7e4016c7d8bcb49020
SENAC Thomas <t.senac@rubika-edu.com>
2020-01-10 18:30:47 +0100

    avancement spawner

--- GhostHunters/Assets/Scripts/Spawner scripts/Spawner.cs
+++ GhostHunters/Assets/Scripts/Spawner scripts/Spawner.cs
@@ -4,8 +4,57 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField]
-    GameObject[] Ghosts;
+    public List<WaveArrival> numberOfWaves;
 
-    public List<List<string>> letmeread = new List<List<string>>(2);
+    private int waveCount = 0;
+    private int ghostCount = 0;
+
+    private int numberOfWavesLength;
+    private int numberOfGhostsLength;
+
+    private void Start()
+    {
+        numberOfWavesLength = numberOfWaves.Count;
+    }
+
+    public void CallWave()
+    { 
+        if (waveCount <= numberOfWavesLength)
+        {
+            numberOfGhostsLength = numberOfWaves[waveCount].numberOfGhosts.Count;
+            CallGhost();
+        }
+    }
+
+    public void CallGhost()
+    {
+        
+        if (ghostCount < numberOfGhostsLength)
+        {
+            //attendre le temps
+            //Spawn the ghost
+
+            ghostCount += 1;
+        }
+        else if (ghostCount == numberOfGhostsLength)
+        {
+            waveCount += 1;
+            ghostCount = 0;
+        }
+    }
 }
+
+[System.Serializable]
+public class WaveArrival
+{
+    public List<GhostSpawn> numberOfGhosts;
+}
+
+[System.Serializable]
+public class GhostSpawn
+{
+    public GameObject typeOfGhost;
+    public Transform ghostSpawn;
+    public float timeUntilSpawn;
+}
+
